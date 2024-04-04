23530c413605b9f16d7935da1743380dea809ce5
Art Mills <art@hamcois.com>
2024-04-04 15:32:23 -0500

    Dave Ryan Python course

---
+++ lesson03/meaning.py
@@ -0,0 +1,10 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('The meaning of life is 42')
+# else:
+#     print('The meaning of life is not 42')
+
+# ternary operator does same as the if else statement above
+print('The meaning of life is 42') if meaning > 10 else print('The meaning of life is not 42')
